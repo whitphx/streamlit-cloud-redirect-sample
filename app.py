3c078284fd8f7a58d5ceb70a0e03239f8a9c9293
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-06-26 22:43:22 +0900

    Replace components.html() with st.markdown because the component API creates iframe but it's not good for the parent page redirect

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import streamlit as st
-import streamlit.components.v1 as components
 
 st.title("Redirect to Google after 5 seconds")
 
-components.html(
-    """
+st.markdown("""
     <script>
     setTimeout(function() {
         window.location.href = 'https://www.google.com';
     }, 5000);
     </script>
-    """,
-    height=0,
-)
+""", unsafe_allow_html=True)
