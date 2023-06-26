531e6c8feaebefc1ff7c829a76443f338f8291fd
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-06-26 22:45:51 +0900

    Debug

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ import streamlit as st
 st.title("Redirect to Google after 5 seconds")
 
 st.markdown("""
+<div>
+    <p>Lorem ipsum</p>
     <script>
+    console.log("The JavaScript code is executed!");
+
     setTimeout(function() {
         window.location.href = 'https://www.google.com';
     }, 5000);
     </script>
+</div>
 """, unsafe_allow_html=True)
