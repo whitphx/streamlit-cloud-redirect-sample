25906a9a9fb55ebdf26697c66f7fe84d98a766a0
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-06-26 23:00:28 +0900

    Test links

--- app.py
+++ app.py
@@ -3,6 +3,18 @@ import streamlit as st
 st.title("Redirect to Google after 5 seconds")
 
 st.markdown("""
+[Click this link](https://www.google.com)
+""")
+
+st.markdown("""
+[Click this link](https://www.google.com){:target="_blank" rel="noreferrer noopener"}
+""")
+
+st.markdown("""
+<a href="https://www.google.com" target="_blank" rel="noreferrer noopener">Click this link</a>
+""", unsafe_allow_html=True)
+
+st.markdown("""
 <div onload="alert('foo')">
     <p>Lorem ipsum</p>
     <script>
